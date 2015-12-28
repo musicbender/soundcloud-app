aea5ba41ea61dd23179ed4cfc427db15bb1952d6
musicbender <musicbender@gmail.com>
2015-12-28 11:53:56 -0800

    connected and athenticated api. Can show data on user

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
 SC.initialize({
     client_id: 'a025d7ac04682146e46fab7dec6d02bd',
-    redirect_uri: 'callback.html'
+    redirect_uri: 'http://127.0.0.1:49392/callback.html'
 });
 
 $(document).ready(function(){
 
-    $('a.connect').click(function(e){
+    $('.connect').click(function(e){
         console.log('clicked');
         e.preventDefault();
         SC.connect(function(){
             SC.get('/me', function(me){
+                console.log(me);
                 $('#username').text(me.username);
+                $('#first-name').text(me.first_name);
+                $('#last-name').text(me.last_name);
+                $('#city').text(me.city);
+                $('#track-count').text(me.track_count);
+                $('#sc-plan').text(me.plan);
             });
         });
     });
