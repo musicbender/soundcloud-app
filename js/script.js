edbc8bf5a3afd4b5539dc568399e448dbb5e83f0
musicbender <musicbender@gmail.com>
2015-12-28 10:28:46 -0800

    initialized soundcloud API

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+SC.initialize({
+    client_id: 'a025d7ac04682146e46fab7dec6d02bd',
+    redirect_uri: 'callback.html'
+});
+
 $(document).ready(function(){
+
+    $('a.connect').click(function(e){
+        console.log('clicked');
+        e.preventDefault();
+        SC.connect(function(){
+            SC.get('/me', function(me){
+                $('#username').text(me.username);
+            });
+        });
+    });
+
+    
     
 });
